8ae1d9b6a03d56947ebe1a7a2dbc691e63d08f5f
codekng <115104833+codekng@users.noreply.github.com>
2025-05-31 11:04:15 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -170,7 +170,7 @@ export default function LandingPage() {
   )
 }
           {/* Scroll indicator */}
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+          <div>
             <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center">
               <div className="w-1 h-3 bg-white/70 rounded-full mt-2 animate-pulse"></div>
             </div>
